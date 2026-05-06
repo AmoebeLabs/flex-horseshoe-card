e09d5f6023455f63f5f9a177d06722138290ad8b
ZoefdeHaas <amoebelabs@users.noreply.github.com>
2026-05-06 13:39:34 +0200

    Create custom TrustedType for horseshoe card lit-html package

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import json from '@rollup/plugin-json';
 import serve from 'rollup-plugin-serve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
+import replace from '@rollup/plugin-replace';
 
 const dev = process.env.ROLLUP_WATCH || process.env.DEV;
 
@@ -17,6 +18,18 @@ const serveopts = {
     'Access-Control-Allow-Origin': '*',
   },
 };
+
+const litTrustedTypesPatch = replace({
+  preventAssignment: true,
+  include: [
+    'node_modules/lit-html/**/*.js',
+    'node_modules/lit-html/*.js',
+  ],
+  values: {
+    "'lit-html'": "'flex-horseshoe-card-lit-html'",
+    '"lit-html"': '"flex-horseshoe-card-lit-html"',
+  },
+});
 
 export default {
   input: 'src/main.js',
@@ -39,6 +52,13 @@ export default {
     exclude: 'node_modules/**',
   },
   plugins: [
+  replace({
+    preventAssignment: true,
+    values: {
+      'createPolicy("lit-html"': 'createPolicy("flex-horseshoe-card-lit-html"',
+      "createPolicy('lit-html'": "createPolicy('flex-horseshoe-card-lit-html'",
+    },
+  }),
     commonjs(),
     //   json({
     //   include: 'package.json',
